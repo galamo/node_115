1d3f2d3fb8a5aea616473a3c7bfc1634ceb7105e
shaharsol <shahar@gnift.com>
2025-04-09 19:57:39 +0300

    a

---
+++ 9.4/index.js
@@ -0,0 +1,5 @@
+function sayHi() {
+    console.log("Hello my name is Yam and i am Maccabi Haifa Fan")
+}
+
+sayHi()
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Node.js install
+
+https://nodejs.org/en
+
+# Mac
+- brew install node
