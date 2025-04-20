9e616377d42ea163fb8e435cc3618ef87080b11f
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2025-04-20 18:29:28 +0300

    a

--- 9.4/server.mjs
+++ 9.4/server.mjs
@@ -1,14 +1,25 @@
 // server.mjs
-import { createServer } from 'node:http';
+import { createServer } from "node:http";
 
 const server = createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!\n');
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(
+    "Hello Class Nodejs!" +
+      new Date().toISOString() +
+      " U are user number " +
+      Math.random() * 9999
+  );
 });
 
+// 8080 http
+// 80 http
+
+// 443 https
+// hyper text transfer protocol secured
+
 // starts a simple http server locally on port 3000
-server.listen(3006, '127.0.0.1', () => {
-    console.log('Listening on 127.0.0.1:3000');
+server.listen(3000, "127.0.0.1", () => {
+  console.log("Listening on 127.0.0.1:3000");
 });
 
 // run with `node server.mjs`
