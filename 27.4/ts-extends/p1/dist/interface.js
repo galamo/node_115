3a49dc208635291d80fd15ae890e732a3f3606a1
shaharsol <shahar@gnift.com>
2025-04-27 19:44:34 +0300

    a

--- 27.4/ts-extends/p1/dist/interface.js
+++ 27.4/ts-extends/p1/dist/interface.js
@@ -1,4 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.persons = void 0;
+exports.logPerson = logPerson;
 const u1 = getUser(23435);
 function getUser(id) {
     return { id: 111, name: "eyal", email: "eyallevi@gmail.com", age: 28, dbId: "aa" };
@@ -7,3 +10,31 @@ const eyalIsAdmin = { id: 111, name: "eyal", email: "eyallevi@gmail.com", age: 2
 function addUser(u) {
     console.log(u);
 }
+const myCardIntersection = { header: "new card", color: "black", height: 1, width: 2, src: "source" };
+const myCardUnion = { header: "eader", color: "aaa" };
+exports.persons = [
+    {
+        name: "Max Mustermann",
+        age: 25,
+        occupation: "Chimney sweep",
+    },
+    {
+        name: "Jane Doe",
+        age: 32,
+        role: "Administrator",
+    },
+    {
+        name: "Kate Müller",
+        age: 23,
+        occupation: "Astronaut",
+    },
+    {
+        name: "Bruce Willis",
+        age: 64,
+        role: "World saver",
+    },
+];
+function logPerson(user) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+exports.persons.forEach(logPerson);
--- 27.4/ts-extends/p1/src/interface.ts
+++ 27.4/ts-extends/p1/src/interface.ts
@@ -1,3 +1,5 @@
+import { ExitStatus } from "typescript"
+
 interface User {
     id: number,
     email: string,
@@ -28,3 +30,62 @@ function addUser(u: User) {
 }
 
 
+interface Card {
+    header: string,
+    color: string
+}
+
+interface CustomImage {
+    height: number,
+    width: number,
+    src: string
+}
+
+const myCardIntersection: CustomImage & Card = { header: "new card", color: "black", height: 1, width: 2, src: "source" }
+const myCardUnion: CustomImage | Card = { header: "eader", color: "aaa" }
+
+
+// # Ex 
+
+interface User {
+    name: string;
+    age: number;
+    occupation: string;
+  }
+  
+  interface Admin {
+    name: string;
+    age: number;
+    role: string;
+  }
+  
+  export type Person = unknown; // THIS IS WRONG!!!
+  
+  export const persons: User[] = [
+    {
+      name: "Max Mustermann",
+      age: 25,
+      occupation: "Chimney sweep",
+    },
+    {
+      name: "Jane Doe",
+      age: 32,
+      role: "Administrator",
+    },
+    {
+      name: "Kate Müller",
+      age: 23,
+      occupation: "Astronaut",
+    },
+    {
+      name: "Bruce Willis",
+      age: 64,
+      role: "World saver",
+    },
+  ];
+  
+  export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+  }
+  
+  persons.forEach(logPerson);
