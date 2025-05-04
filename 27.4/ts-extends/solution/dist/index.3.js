0a007a050ae71a353000baadb1cdba96d695647a
shaharsol <shahar@gnift.com>
2025-05-04 19:03:27 +0300

    a

--- 27.4/ts-extends/solution/dist/index.3.js
+++ 27.4/ts-extends/solution/dist/index.3.js
@@ -2,6 +2,10 @@
 function wrapInArray(value) {
     return [value];
 }
+const p = { id: 11, name: "aaa", price: 11, inStock: true };
+const result1 = wrapInArray(4);
+const result2 = wrapInArray("Sttt");
+const result3 = wrapInArray(p);
 function getItemWithId(value, a, b) {
     if (!a)
         return;
@@ -11,4 +15,45 @@ const t1 = document.querySelector("aaa");
 const t2 = document.querySelectorAll("ccc");
 const data = Array.from(t2);
 const header = document.querySelector("HTMLaaa");
-function myQuerySelector(v) { } // implement the following function using document.querySelector support Generic
+// implement the following function using document.querySelector support Generic
+function myQuerySelector(idValue) {
+    const item = document.querySelector(idValue);
+    if (!item)
+        return {};
+    return item;
+}
+function getItemWithId2(value) {
+    if (!value)
+        return;
+    return value;
+}
+//0 false "" undienfed, null 
+function getItemWithId3(value) {
+    if (!value)
+        return;
+    return value;
+}
+getItemWithId2(0);
+getItemWithId2("");
+const t = getItemWithId3(false);
+myQuerySelector("someId");
+var Entities;
+(function (Entities) {
+    Entities["User"] = "User";
+    Entities["Car"] = "Car";
+    Entities["Store"] = "Store";
+})(Entities || (Entities = {}));
+var Errors;
+(function (Errors) {
+    Errors[Errors["ServerError"] = 1] = "ServerError";
+    Errors[Errors["ClientError"] = 2] = "ClientError";
+    Errors[Errors["UserError"] = 3] = "UserError";
+})(Errors || (Errors = {}));
+function getData(entity) {
+    // server request
+    // @ts-ignore
+    return 1;
+}
+const stores = getData(Entities.Store);
+const users = getData(Entities.User);
+const cars = getData(Entities.Car);
--- 27.4/ts-extends/solution/src/index.3.ts
+++ 27.4/ts-extends/solution/src/index.3.ts
@@ -1,7 +1,16 @@
 function wrapInArray<T>(value: T): [T] {
     return [value]
 }
-
+type Product = {
+    id: number;
+    name: string,
+    price: number,
+    inStock: boolean
+}
+const p: Product = { id: 11, name: "aaa", price: 11, inStock: true }
+const result1 = wrapInArray(4)
+const result2 = wrapInArray("Sttt")
+const result3 = wrapInArray(p)
 
 function getItemWithId<ParamType1, ParamType2>(value: ParamType1, a: number, b: ParamType2): { key1: ParamType1, key2: ParamType2 } | undefined {
     if (!a) return;
@@ -14,4 +23,63 @@ const t2 = document.querySelectorAll<SVGElement>("ccc")
 const data = Array.from(t2)
 const header = document.querySelector<HTMLHeadElement>("HTMLaaa")
 
-function myQuerySelector(v: string) { } // implement the following function using document.querySelector support Generic
+// implement the following function using document.querySelector support Generic
+function myQuerySelector<T>(idValue: string): T {
+    const item = document.querySelector(idValue)
+    if (!item) return {} as T
+    return item;
+
+}
+
+
+function getItemWithId2(value: string | number | undefined | null): string | boolean | number | undefined | null {
+    if (!value) return;
+    return value
+}
+
+//0 false "" undienfed, null 
+function getItemWithId3<T>(value: T): T | undefined {
+    if (!value) return;
+    return value
+}
+getItemWithId2(0)
+getItemWithId2("")
+const t = getItemWithId3<boolean>(false)
+
+myQuerySelector<HTMLAnchorElement>("someId")
+
+
+
+// Car, User, Store 
+type Car = { color: string, lp: string, year: number }
+type User = { userName: string, location: string, password: string }
+type Store = { location: string, address: string, employees: User[] }
+
+enum Entities {
+    User = "User",
+    Car = "Car",
+    Store = "Store"
+}
+
+
+enum Errors {
+    ServerError = 1,
+    ClientError = 2,
+    UserError = 3
+}
+
+function getData<T>(entity: Entities): (T & { id: string })[] {
+
+    // server request
+    // @ts-ignore
+    return 1;
+
+}
+
+
+const stores = getData<Store>(Entities.Store)
+const users = getData<User>(Entities.User)
+const cars = getData<Car>(Entities.Car)
+
+
+
