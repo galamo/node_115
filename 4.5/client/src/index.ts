88000c01ad3259a67f93e6b50aa69ae1b0376bbc
shaharsol <shahar@gnift.com>
2025-05-04 21:31:58 +0300

    a

---
+++ 4.5/client/src/index.ts
@@ -0,0 +1,9 @@
+async function getAllCustomers() {
+    const result = await fetch("http://localhost:3000/customers")
+    const data = await result.json()
+    const resultDiv = document.getElementById("getAllCustomersResult");
+    if (resultDiv) {
+        resultDiv.innerText = JSON.stringify(data)
+    }
+}
+
